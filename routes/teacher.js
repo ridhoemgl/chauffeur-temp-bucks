caebe7f880994ee3a162f0e5181a6bec0f4bf52a
ridhoemgl <ridhoemgl@gmail.com>
2018-08-30 20:19:10 +0700

    deleted routes/teacher.js file

--- routes/teacher.js
+++
@@ -1,8 +0,0 @@
-const app = require('express').Router()
-const teacherController = require('../controller/teacherController')
-
-app.get('/', (req, res) => {
-    res.send('helo teacher')
-})
-
-module.exports = app
